298917e3508c25306a8d871be27de7f14110fd0d
Gonçalo Rosa <goncalojmrosa@gmail.com>
2024-08-29 18:09:24 +0100

    feat: add confetti effect on task complete

--- src/components/task/taskItem.tsx
+++ src/components/task/taskItem.tsx
@@ -3,16 +3,17 @@ import { Package, PackageOpen } from "lucide-react";
 interface TaskItemProps {
   id: number;
   title: string;
+  handleComplete: (id: number) => void;
 }
 
-export function TaskItem({ id, title }: TaskItemProps) {
+export function TaskItem({ id, title, handleComplete }: TaskItemProps) {
   return (
     <div
       key={id}
       className="h-[60px] border-b border-slate-400/10 relative group flex items-center justify-between"
     >
       <span>{title}</span>
-      <div>
+      <div onClick={() => handleComplete(id)}>
         <Package className="text-slate-500 group-hover:hidden" />
         <PackageOpen className="text-slate-500 hidden group-hover:block group-hover:scale-125" />
       </div>
--- src/components/task/taskList.tsx
+++ src/components/task/taskList.tsx
@@ -1,17 +1,20 @@
 "use client";
 
-import { CirclePlus, Package, PackageOpen } from "lucide-react";
+import { CirclePlus } from "lucide-react";
 import { TaskItem } from "./taskItem";
 import { useState } from "react";
+import { default as Confetti } from "react-confetti";
+import useWindowDimensions from "@/hooks/windowSize";
 
 type Task = {
   id: number;
   title: string;
 };
 
-
 export default function TaskList() {
+  const { width, height } = useWindowDimensions();
   const [isAdding, setIsAdding] = useState(false);
+  const [taskCompleted, setTaskComplete] = useState(false);
   const [tasks, setTasks] = useState<Task[]>([
     { id: 0, title: "Task 0" },
     { id: 1, title: "Task 1" },
@@ -28,9 +31,29 @@ export default function TaskList() {
     setTasks([...tasks, { id: tasks.length, title: "Task " + tasks.length }]);
     setIsAdding(false);
   }
+  function handleCompleteTask(id: number) {
+    setTasks(tasks.filter((task) => task.id !== id));
+    setTaskComplete(true);
+  }
+
+  if (!(width && height)) {
+    return null;
+  }
+
   return (
     <div className="m-4">
-      <div className="h-[60px] flex items-center justify-center gap-2 border-2 rounded cursor-pointer border-dashed border-spacing-10" onClick={() => setIsAdding(true)}>
+      {taskCompleted && (
+        <Confetti
+          width={width}
+          height={height}
+          recycle={false}
+          onConfettiComplete={() => setTaskComplete(false)}
+        />
+      )}
+      <div
+        className="h-[60px] flex items-center justify-center gap-2 border-2 rounded cursor-pointer border-dashed border-spacing-10"
+        onClick={() => setIsAdding(true)}
+      >
         <span>
           <CirclePlus />
         </span>
@@ -38,11 +61,20 @@ export default function TaskList() {
       </div>
       {isAdding && (
         <form onSubmit={handleAddTask} className="flex items-center mt-4 h-7">
-          <input type="text" placeholder="New Task" className="bg-transparent flex-1 focus:outline-none" />
+          <input
+            type="text"
+            placeholder="New Task"
+            className="bg-transparent flex-1 focus:outline-none"
+          />
         </form>
       )}
       {tasks.map((task) => (
-        <TaskItem key={task.id} id={task.id} title={task.title} />
+        <TaskItem
+          key={task.id}
+          id={task.id}
+          title={task.title}
+          handleComplete={handleCompleteTask}
+        />
       ))}
     </div>
   );
